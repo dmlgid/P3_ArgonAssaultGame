e9296fac677a2e126ba8d584d83bbbc8a046427f
hyang <quddkfl5109@naver.com>
2024-06-27 21:41:32 +0900

    Update : scoreboard and enemy

--- ArgonAssaultGame/Assets/02.Scripts/EnemyController.cs
+++ ArgonAssaultGame/Assets/02.Scripts/EnemyController.cs
@@ -4,33 +4,51 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] GameObject explosion;
+    [SerializeField] GameObject explosion_VFX;
+    [SerializeField] GameObject hit_VFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
+    [SerializeField] int lifeCount = 3;
 
     ScoreBoard scoreBoard;
+    Rigidbody rb;
 
     private void Start()
     {
         //Find는 성능이 안좋지만, 적을 하나씩 생성할때마다 하나의 객체 타입을 찾는건 비용이 가볍다.
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        AddRigidbody();
     }
 
-    private void OnParticleCollision(GameObject other)
+    private void AddRigidbody()
+    {
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.isKinematic = true;
+    }
+
+    private void OnParticleCollision()
     {
         // Debug.Log($"{this.name} -- Collision -- {other.gameObject.name}");
         ProcessHit();
-        KillEnemy();
+
+        if (lifeCount <= 0)
+        {
+            KillEnemy();
+        }
     }
 
     private void ProcessHit()
     {
+        GameObject explo = Instantiate(hit_VFX, transform.position, Quaternion.identity);
+        explo.transform.parent = parent;
         scoreBoard.IncreaseScore(scorePerHit);
+        lifeCount--;
     }
 
     private void KillEnemy()
     {
-        GameObject explo = Instantiate(explosion, transform.position, Quaternion.identity);
+        GameObject explo = Instantiate(explosion_VFX, transform.position, Quaternion.identity);
         explo.transform.parent = parent;
         Destroy(this.gameObject);
     }
--- ArgonAssaultGame/Assets/02.Scripts/ScoreBoard.cs
+++ ArgonAssaultGame/Assets/02.Scripts/ScoreBoard.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "START";
+    }
 
     public void IncreaseScore(int amountScore)
     {
         score += amountScore;
-        Debug.Log($"Score is now : {score}");
+        scoreText.text = score.ToString();
     }
 
 }
